03fd8b5b104a7dd9a7e7feb8f005c85a2c4150d0
Michael <100490812+DebSol11@users.noreply.github.com>
2025-07-15 18:40:42 +0200

    Add factory boilerplate code

---
+++ script.js
@@ -0,0 +1,9 @@
+let gameboardObject = {};
+let playerObject = [{}];
+
+function createUser (name) {
+  const discordName = "@" + name;
+  return { name, discordName };
+}
+// and that's very similar, except since it's just a function,
+// we don't need a new keyword
