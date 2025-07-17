78c72f7c786242bd52a754ee8a6121bdde337646
Michael <100490812+DebSol11@users.noreply.github.com>
2025-07-17 14:28:28 +0200

    Set up basic code examples and frames

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-let gameboardObject = {
-  // gameboard.dropToken() method
+// Global objects
+let gameBoard = {
+  // gameBoard.dropToken() method
   dropToken: function() {
     console.log("The dropToken test");
   }
 };
-let playerObject = [{}];
-let gameObject = {};
+let player = [{}, {}, {}];
+let game = {};
+
 let gameController = {
   // gameController.playRound() method
   playRound: function() {
-
   }
 };
 
 // THE MODULE PATTERN: IIFEs = Immediately Invoked Function Expression: Visual representation of the game
 // This module will leverage an updateScreen pattern. The purpose of this pattern is to take some data about our game, such as the state of the game board and which player's turn it is, and update the screen each time a player takes their turn.
 const ScreenController = (function () {
-// Your code goes here 
-//   const add = (a, b) => a + b;
-//   const sub = (a, b) => a - b;
-//   const mul = (a, b) => a * b;
-//   const div = (a, b) => a / b;
-//   return { add, sub, mul, div };
+  const x = "";
+  const y = "";
+  return {x,y};
 })();
 
 // FACTORY FUNCTION Example
@@ -32,7 +30,7 @@ const ScreenController = (function () {
 
 // Nested-loop technique to create a 2d array.
 // Initialize it with the indices of rows and columns
-function gameboard() {
+function createGameBoard() {
   const rows = 3;
   const columns = 3;
   const board = [];
@@ -48,7 +46,7 @@ function gameboard() {
   return board;
 }
 
-gameboard();
+createGameBoard();
 
 
 
