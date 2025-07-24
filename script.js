2cf086d8cfb1c99c04227868ef2e9c2d7edeab2d
Michael <100490812+DebSol11@users.noreply.github.com>
2025-07-24 13:31:44 +0200

    Make alternating input work and display in console

--- script.js
+++ script.js
@@ -173,7 +173,36 @@ function displayBoard() {
       let individualField = document.createElement("p");
       individualField.id = counter;
       individualField.setAttribute("class", "individualField");
-      individualField.textContent = board[i][j];
+      individualField.addEventListener("click", (e) => {
+        e.target.innerHTML = sign;
+        if (individualField.id == 0) {
+          board[0][0] = sign;
+        } else if (individualField.id == 1) {
+          board[0][1] = sign;
+        } else if (individualField.id == 2) {
+          board[0][2] = sign;
+        } else if (individualField.id == 3) {
+          board[1][0] = sign;
+        } else if (individualField.id == 4) {
+          board[1][1] = sign;
+        } else if (individualField.id == 5) {
+          board[1][2] = sign;
+        } else if (individualField.id == 6) {
+          board[2][0] = sign;
+        } else if (individualField.id == 7) {
+          board[2][1] = sign;
+        } else if (individualField.id == 8) {
+          board[2][2] = sign;
+        } 
+        if (sign == "O") {
+          sign = "X";
+        } else {
+          sign = "O";
+        }
+        
+        console.log(individualField.id);
+        console.log(board);
+      });
       boardContainer.appendChild(individualField);
     }
   }
@@ -184,11 +213,9 @@ displayBoard();
 function createSelectorsForFields() {
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      console.log()
-
+      console.log();
     }
   }
 }
 
 // createSelectorsForFields();
-
